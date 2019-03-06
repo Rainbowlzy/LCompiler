7f173d2dbe68f98f087b5fb21303e476ba68ba2e
Rainbow Lu <rainbowluzy@hotmail.com>
2019-03-06 14:39:23 +0800

    添加项目文件。

---
+++ LCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
